602c1c82c78f2f57809165473f40812b04d86313
chenxianqi <361554012@qq.com>
2020-03-15 15:45:46 +0800

    add gitti

--- robot/handler_status.go
+++ robot/handler_status.go
@@ -15,7 +15,7 @@ func NewStatusHandler(appAccount string) *StatusHandler {
 // HandleChange handleChange
 func (c StatusHandler) HandleChange(isOnline bool, errType, errReason, errDescription *string) {
 	if isOnline {
-		fmt.Printf("机器人:%v 霸道上线 status changed: online.", c.appAccount)
+		fmt.Printf("机器人:%v 霸道上线 status changed: online. \r\n", c.appAccount)
 	} else {
 		// 有机器人掉线，重新登录
 		fmt.Printf("[机器人:%v 挂掉了] status changed: offline，errType:%v, errReason:%v, errDes:%v\r\n", c.appAccount, *errType, *errReason, *errDescription)
